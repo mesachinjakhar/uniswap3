7411ea6c6978d6f8e02c8e27bbd5700f202cb80f
mesachinjakhar <111659417+mesachinjakhar@users.noreply.github.com>
2024-05-30 17:18:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Web3 = require('web3');
-const { Token } = require('@uniswap/sdk-core');
+const { Token, TradeType, Route, Trade, Fetcher } = require('@uniswap/sdk');
 const { abi: QuoterABI } = require('@uniswap/v3-periphery/artifacts/contracts/lens/Quoter.sol/Quoter.json');
+const { abi: SwapRouterABI } = require('@uniswap/v3-periphery/artifacts/contracts/interfaces/ISwapRouter.sol/ISwapRouter.json');
+const { abi: FactoryABI } = require('@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Factory.sol/IUniswapV3Factory.json');
 
 const config = {
     DEFAULT_API_PORT: 5001,
@@ -8,14 +10,16 @@ const config = {
     UNISWAPV3_FACTORY_ADDRESS: "0x1F98431c8aD98523631AE4a59f267346ea31F984",
     UNISWAPV3_QUOTER_ADDRESS: "0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6",
     WETH_ADDRESS_MAINNET: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+    DAI_ADDRESS_MAINNET: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
     CUSTOM_AMOUNT: "1000000000000000000" // 1 ETH in wei
 };
 
 const web3 = new Web3(new Web3.providers.WebsocketProvider(config.DEFAULT_NODE_URL));
 
 const WETH = new Token(1, config.WETH_ADDRESS_MAINNET, 18, 'WETH', 'Wrapped Ether');
-const DAI = new Token(1, '0x6B175474E89094C44Da98b954EedeAC495271d0F', 18, 'DAI', 'Dai Stablecoin');
+const DAI = new Token(1, config.DAI_ADDRESS_MAINNET, 18, 'DAI', 'Dai Stablecoin');
 const quoter = new web3.eth.Contract(QuoterABI, config.UNISWAPV3_QUOTER_ADDRESS);
+
 const amountIn = config.CUSTOM_AMOUNT;
 const poolFees = [500, 3000, 10000]; // 0.05%, 0.30%, 1% fee tiers
 
@@ -24,7 +28,7 @@ const fetchPrices = async () => {
         try {
             const amountOut = await quoter.methods.quoteExactInputSingle(
                 config.WETH_ADDRESS_MAINNET,
-                DAI.address,
+                config.DAI_ADDRESS_MAINNET,
                 fee, // Pool fee
                 amountIn,
                 0
