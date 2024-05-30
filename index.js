173a9bb3c0a3b134649ba12a9ab635894ac7e74a
mesachinjakhar <111659417+mesachinjakhar@users.noreply.github.com>
2024-05-31 01:31:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const { Token, TradeType, TokenAmount, Percent, Route, Fetcher } = require('@uniswap/v3-sdk');
+const { Token, TradeType, TokenAmount, Percent, Route } = require('@uniswap/sdk-core');
+const { Fetcher } = require('@uniswap/v3-sdk');
 const { abi: IUniswapV3PoolABI } = require('@uniswap/v3-core/artifacts/contracts/UniswapV3Pool.sol/UniswapV3Pool.json');
 const ethers = require('ethers');
 
