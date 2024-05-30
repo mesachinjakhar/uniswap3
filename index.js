000a8113dc76d9a1af90903921f258abf6ee1365
mesachinjakhar <111659417+mesachinjakhar@users.noreply.github.com>
2024-05-30 19:38:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { ethers } = require('ethers');
-const { ChainId, Token, TokenAmount, Route, Trade, TradeType } = require('@uniswap/sdk');
+const { ChainId, Token, TokenAmount, Route, Trade, TradeType, Pair } = require('@uniswap/sdk');
 
 require('dotenv').config();
 
@@ -11,21 +11,11 @@ const main = async () => {
     const WETH = new Token(ChainId.MAINNET, '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', 18, 'WETH', 'Wrapped Ether');
     const DAI = new Token(ChainId.MAINNET, '0x6B175474E89094C44Da98b954EedeAC495271d0F', 18, 'DAI', 'Dai Stablecoin');
 
-    const pair = new Route([{
-      token: WETH,
-      liquidity: '1000000000000000000', // 1 ETH in wei
-    }, {
-      token: DAI,
-      liquidity: '1000000000000000000000', // Dummy liquidity for DAI, adjust as per actual liquidity
-    }], WETH);
+    const pair = new Pair(new TokenAmount(WETH, '1000000000000000000'), new TokenAmount(DAI, '1000000000000000000000'));
 
-    const trade = new Trade(pair, new TokenAmount(WETH, '1000000000000000000'), TradeType.EXACT_INPUT);
+    const route = new Route([pair], WETH);
 
-    console.log(`Best price for swapping 1 ETH to DAI: ${trade.executionPrice.toSignificant(6)} DAI`);
-  } catch (error) {
-    console.error('Error:', error);
-  }
-};
+    const trade = new Trade(route, new TokenAmount(WETH, '1000000000000000000'), TradeType.EXACT_INPUT);
 
-main().catch(console.error);
+    console.log(`Bes
 
