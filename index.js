cc45c5606b2dec2c80b45907e5b23a82f49b4cc2
mesachinjakhar <111659417+mesachinjakhar@users.noreply.github.com>
2024-05-31 20:30:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const INFURA_TEST_URL = process.env.ERIGON_URL
 
 const web3Provider = new ethers.providers.JsonRpcProvider(INFURA_TEST_URL) // Ropsten
 
-const chainId = 1
+const chainId = 3
 const router = new AlphaRouter({ chainId: chainId, provider: web3Provider})
 
 const name0 = 'Wrapped Ether'
@@ -21,7 +21,7 @@ const address0 = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
 const name1 = 'DAI Token'
 const symbol1 = 'DAI'
 const decimals1 = 18
-const address1 = '0x6B175474E89094C44Da98b954EedeAC495271d0F'
+const address1 = '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984'
 
 const WETH = new Token(chainId, address0, decimals0, symbol0, name0)
 const UNI = new Token(chainId, address1, decimals1, symbol1, name1)
