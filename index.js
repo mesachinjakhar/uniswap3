73e8f256a76b8c819cbad18493c9be39232ca287
Sachin Jakhar <111659417+mesachinjakhar@users.noreply.github.com>
2024-06-01 18:05:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const JSBI = require('jsbi')
 const { TickMath, FullMath } = require('@uniswap/v3-sdk')
 
-const baseToken = '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599' // WETH
+const baseToken = '0x6B175474E89094C44Da98b954EedeAC495271d0F' // WETH
 const quoteToken = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2' // WBTC
 
 async function main(
